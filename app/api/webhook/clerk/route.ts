86297fb9bca8b0aa14e9be4f6ed1444e63f55296
vanquy1410 <quynguyenpro1410@gmail.com>
2024-07-03 09:16:34 +0700

    create user

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,9 +1,15 @@
+// import { Webhook } from 'svix'
+// import { headers } from 'next/headers'
+// import { WebhookEvent } from '@clerk/nextjs/server'
+// import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
+// import { clerkClient } from '@clerk/nextjs/server'
+// import { NextResponse } from 'next/server'
+
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
+import { WebhookEvent, clerkClient } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
 import { Webhook } from 'svix'
-import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
-import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
-import { clerkClient } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
  
 export async function POST(req: Request) {
  
@@ -58,13 +64,13 @@ export async function POST(req: Request) {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-        clerkId: id,
-        email: email_addresses[0].email_address,
-        username: username!, // Assuming username is always present; otherwise, handle similarly
-        firstName: first_name || '', // Provide a default empty string if null
-        lastName: last_name || '', // Provide a default empty string if null
-        photo: image_url,
-      }
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name!,
+      lastName: last_name!,
+      photo: image_url,
+    }
 
     const newUser = await createUser(user);
 
@@ -83,13 +89,13 @@ export async function POST(req: Request) {
     const {id, image_url, first_name, last_name, username } = evt.data
 
     const user = {
-        firstName: first_name ?? "", // Provide a default value if first_name is null
-        lastName: last_name ?? "", // Provide a default value if last_name is null
-        username: username!, // Assuming username is always a string and not null
-        photo: image_url,
-      }
-      
-      const updatedUser = await updateUser(id, user);
+      firstName: first_name!,
+      lastName: last_name!,
+      username: username!,
+      photo: image_url,
+    }
+
+    const updatedUser = await updateUser(id, user)
 
     return NextResponse.json({ message: 'OK', user: updatedUser })
   }
--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,20 +1,20 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const MONG0DB_URI =process.env.MONG0DB_URI ;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || { conn : null, promise: null};
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
-export const connetToDatabase = async () => {
-    if (cached.conn) return cached.conn;
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
 
-    if(!MONG0DB_URI) throw new Error('MONGODB_URI is missing');
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-    cached.promise = cached.promise || mongoose.connect(MONG0DB_URI, {
-        dbName: 'star seminar',
-        bufferCommands: false,
-    })
-    cached.conn = await cached.promise;
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'evently',
+    bufferCommands: false,
+  })
 
-    return cached.conn;
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
-
