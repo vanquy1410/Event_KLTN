44368d1d7c461793b0f2f383ebf0efbead389a0e
vanquy1410 <quynguyenpro1410@gmail.com>
2024-07-03 14:33:07 +0700

    fix

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -59,8 +59,8 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name!,
-      lastName: last_name!,
+      firstName: first_name?? '',
+      lastName: last_name?? '',
       photo: image_url,
     };
 
@@ -81,8 +81,8 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name!,
-      lastName: last_name!,
+      firstName: first_name?? '',
+      lastName: last_name?? '',
       username: username!,
       photo: image_url,
     };
